f8b59ded7ec7821718467aaa85471af722541b97
ule270 <u.le270@gmail.com>
2023-03-03 19:51:35 -0500

    I have added changes.

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -31,4 +31,21 @@ public class LogginLabTest {
             }
         }
     }
+    @org.junit.Test
+    public void thresholdReached() {
+        Integer finalLimit = 5;
+
+        LogginLab lab = new LogginLab();
+        lab.setThreshold(finalLimit);
+
+        for (Integer i = 1; i <= finalLimit + 1; i++) {
+            if (lab.thresholdReached(i)) {
+                logger.log(Level.INFO, "Threshold finally reached!");
+                assertTrue(lab.thresholdReached(i));
+            } else {
+                logger.log(Level.INFO, "Threshold not reached! It is " + i);
+                assertFalse(lab.thresholdReached(i));
+            }
+        }
+    }
 }
